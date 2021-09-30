2434512d5e3522a9f4ae277df6293c9a616646a1
Lashonda Miller <lashondamiller007@gmail.com>
2021-09-30 14:19:56 -0400

    Exceptions, Throw, Catches practice

--- Banking/Program.cs
+++ Banking/Program.cs
@@ -4,16 +4,56 @@ namespace Banking
 {
     class Program
     {
+        static void Method1()
+        {
+            try
+            {
+                Method2();
+            } catch(Exception ex)
+            {
+                Console.WriteLine($"Exception occured: {ex.Message}");
+            }
+        }
+        static void Method2()
+        {
+            Method3();
+        }
+        static void Method3()
+        {
+            var throwException = true;
+            if (throwException)
+            {
+                throw new Exception("My fictitous exception!");
+            }
+        }
         static void Main(string[] args)
         {
-            var sav2 = new Savings2(100);
-            sav2.Description = "Lashonda's Savings2"; // right click, run to cursor, stops executing when it hits a line you right clicked on and selected
-            sav2.InterestRate = 0.12m;
-            sav2.Print();
-            sav2.Deposit(5000);
-            sav2.Withdraw(2000);
-            sav2.CalculateAndPayInterest(1);
-            sav2.Print();
+            Method1();
+
+            //var n = 1;
+            //var d = 0;
+            ////var x = n / d;
+            //try
+            //{
+            //    var x = n / d;
+
+            //}
+            //catch (DivideByZeroException)
+            //{
+            //    Console.WriteLine("Divid by Zero avoided");
+            //} finally
+            //{
+
+            //}
+
+            //var sav2 = new Savings2(100);
+            //sav2.Description = "Lashonda's Savings2"; // right click, run to cursor, stops executing when it hits a line you right clicked on and selected
+            //sav2.InterestRate = 0.12m;
+            //sav2.Print();
+            //sav2.Deposit(5000);
+            //sav2.Withdraw(2000);
+            //sav2.CalculateAndPayInterest(1);
+            //sav2.Print();
 
 
 
